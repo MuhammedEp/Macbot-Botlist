ea220feb6ca3d90f6c3baaf56e2f4893428c5b39
MuhammedEp <101981425+MuhammedEp@users.noreply.github.com>
2025-08-14 01:41:58 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    Bot: {
+        token: "TOKEN",
+        prefix: "?",
+  intents: ["MessageContent","Guilds","GuildMembers","GuildBans","GuildEmojisAndStickers","GuildIntegrations","GuildWebhooks","GuildInvites","GuildVoiceStates","GuildPresences","GuildMessages","GuildMessageReactions","GuildMessageTyping"],
+  events: ["onApplicationCommandPermissionsUpdate","onMessage","onMessageDelete","onMessageUpdate","onMessageDeleteBulk","onJoin","onLeave","onGuildUpdate","onGuildUnavailable","onBanAdd","onBanRemove","onVoiceStateUpdate","onInteractionCreate","onUserUpdate","onFunctionError",],
+  guildOnly: true,
+  respondToBots: false,
+  suppressAllErrors: true,
+  database: {
+        type: "aoi.db",
+        db: require("@aoijs/aoi.db"),
+        dbType: "KeyValue",
+        tables: ["main"],
+        securityKey: "a-32-characters-long-string-here"
+    }
+}}
