c330b8679330ad90d89bd5a95c7882ff9cad0a00
MuhammedEp <101981425+MuhammedEp@users.noreply.github.com>
2025-08-14 01:43:27 +0300

    Update main.js

--- main.js
+++ main.js
@@ -11,21 +11,3 @@ require("./+/variables.js")( client, config )
 
 client.loadCommands("./commands", false)
 
-// HATA LOG
-client.functionErrorCommand({
-  channel: "1101883444386078780",
-  code: `
-$author[$guildName sunucusundan bir hata tespit edildi;$guildIcon]
-$addField[Sunucu ID:;$guildID]
-$addField[Sunucu Sahibi:;$username[$guildOwnerID] ($guildOwnerID)]
-$addField[Fonksiyon:;$handleError[function]]
-$addField[Komut:;$handleError[command]]
-$description[$handleError[error]]
-$color[#2ec6a5]
-$addTimestamp
-$error 
-$addButton[1; ;secondary;silbtn_858951074164244490;false;<:sil:1211313477134848010>]
-   `,
-});
-
-
